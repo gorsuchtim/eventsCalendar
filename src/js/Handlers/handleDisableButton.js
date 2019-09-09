3bf0f9f4ffa41537926aee9745efe03a4d5bb29c
Tim Gorsuch <gorsuchtim@gmail.com>
2019-09-09 11:59:41 -0500

    refactor handle month nav and disable button

--- src/js/Handlers/handleDisableButton.js
+++ src/js/Handlers/handleDisableButton.js
@@ -1,26 +1,28 @@
-'use strict'
+"use strict";
 
-import utilities from '/utilities'
+import utilities from "../Utilities/Utilities";
 
-const disableForwardNav = 11;
-const disableBackwardNav = utilities.getMonth() - 2;
+var disableForwardNav = 11;
+var disableBackwardNav = utilities.getMonth() - 2;
 
-const checkForDisabled = (navButtons) => {
+const checkForDisabled = navButtons => {
   navButtons.forEach(navButton => {
-    if (navButton.getAttribute('disabled')) {
+    if (navButton.getAttribute("disabled")) {
       removeDisabled(navButton);
     }
   });
   return true;
-}
+};
 
-const removeDisabled = navButton => navButton.removeAttribute('disabled');
+const removeDisabled = navButton => navButton.removeAttribute("disabled");
 
-const disableButton = element => element.setAttribute('disabled', 'true');
+const disableButton = element => element.setAttribute("disabled", "true");
 
 const handleDisableButton = (element, navButtons, monthValue) =>
-  checkForDisabled(navButtons) ?
-    monthValue === disableForwardNav || monthValue === disableBackwardNav ? disableButton(element) : false
+  checkForDisabled(navButtons)
+    ? monthValue === disableForwardNav || monthValue === disableBackwardNav
+      ? disableButton(element)
+      : false
     : false;
 
-export default handleDisableButton
+export default handleDisableButton;
