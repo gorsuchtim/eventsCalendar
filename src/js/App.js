a7d54bba5b77da4993c17b2b936d93f71c287f5f
Tim Gorsuch <gorsuchtim@gmail.com>
2019-09-09 11:52:43 -0500

    refactor handle calendar

--- src/js/App.js
+++ src/js/App.js
@@ -8,19 +8,18 @@ window.$ = window.jQuery = jQuery;
 // Import components
 import utilities from "./Utilities/Utilities";
 import handleDomMonths from "./Handlers/handleDomMonths";
-// import handleCalendar from "./Handlers/handleCalendar";
-// import handleValidation from "./Handlers/handleValidation";
+import handleCalendar from "./Handlers/handleCalendar";
 // import setMonthNavEvents from "./Handlers/handleMonthNavigation";
 
 const eventsCalendar = {
-  eventsFilePath: "/js/Components/EventsCalendar/Calendar/",
+  eventsFilePath: "/src/js/Calendar/",
   init() {
     const calendar = document.querySelector(".eventsCalendar__section");
     if (utilities.testForElement(calendar)) {
       if (handleDomMonths()) {
-        console.log("yay");
-        //     if (handleCalendar()) {
-        //       setMonthNavEvents();
+        if (handleCalendar()) {
+          // setMonthNavEvents();
+        }
       }
     }
   }
